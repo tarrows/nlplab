c73db442bc57567f80b86eb9b897352a61644406
c10t <c10t@fa.ke>
2020-07-02 00:10:24 +0900

    add templates

--- main.py
+++ main.py
@@ -3,13 +3,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from starlette.responses import StreamingResponse
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
 def hello():
     return {"hello": "world"}
+
+
+@app.get("/items/{item_id}")
+def read_item(request: Request, item_id: str):
+    context = {"request": request, "item_id": item_id}
+    return templates.TemplateResponse("item.html", context)
+
 
 @app.get("/image")
 async def draw_image():

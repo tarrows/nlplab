343e8a9d32f91ce1bee6ee483accc0bf48370ac3
c10t <c10t@fa.ke>
2020-07-09 23:56:52 +0900

    plot time-series

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import io
+import re
 import pandas as pd
+import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 import numpy as np
 from fastapi import FastAPI, Request
@@ -30,6 +32,31 @@ async def draw_image():
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
     df.plot(ax=ax)
+
+    buf = io.BytesIO()
+    fig.savefig(buf, format='png')
+    buf.seek(0)
+    return StreamingResponse(buf, media_type='image/png')
+
+
+pattern = r'(\d+):(\d+):(\d+)'
+target = re.compile(pattern)
+
+
+@app.get("/time-series/{name}")
+async def draw_time_series(name: str):
+    df = pd.read_csv(f'data/{name}.csv', parse_dates=[0])
+    print(df)
+    df['sec'] = df['time'].map(lambda x: int(target.match(x).group(2)) * 60 + int(target.match(x).group(3)))
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.plot(df['date'], df['sec'])
+
+    daysFmt = mdates.DateFormatter('%Y-%m-%d')
+    ax.xaxis.set_major_formatter(daysFmt)
+    fig.autofmt_xdate()
+
+    print(df[['date', 'sec']])
 
     buf = io.BytesIO()
     fig.savefig(buf, format='png')

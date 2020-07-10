f7e42ab8c4e20248ddb5895cd3ab753135fc54dd
c10t <c10t@fa.ke>
2020-07-10 23:51:55 +0900

    add feature word extraction

---
+++ lib/nlp.py
@@ -0,0 +1,25 @@
+import spacy
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+class NLP(object):
+    def __init__(self, model="en_core_web_sm"):
+        self.nlp = spacy.load(model)
+
+    def tfidf_feature_top(self, docs: list(str), n=10):
+        bow = []
+        for text in docs:
+            doc = self.nlp(text)
+            words = [token.lemma_ for token in doc if token.pos != 'PUNCT' and not token.is_stop]
+            bow.append(words)
+
+        corpus = np.array(bow)
+        tfidf = TfidfVectorizer()
+        x = tfidf.fit_transform(corpus)
+        result = x.toarray()
+        feature_names = np.array(tfidf.get_feature_names())
+        index = result.argsort(axis=1)[:, ::-1]
+        feature_words = [feature_names[row[:n]] for row in index]
+
+        return feature_words
